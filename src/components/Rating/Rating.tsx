2abf0dd76ef0b36b1c03c9e174ca1600f485fa80
j0sephh123 <iossif123gg@gmail.com>
2022-04-25 11:28:06 +0300

    Rating - extract helper fns

--- src/components/Rating/Rating.tsx
+++ src/components/Rating/Rating.tsx
@@ -1,7 +1,13 @@
-import Star from "./Star";
 import classes from "./Rating.module.css";
 import { useState } from "react";
 import { RatingProps } from "../../types";
+import Star from "./Star/Star";
+import {
+  findItemIndex,
+  resetActive,
+  resetClickedAndActive,
+  setClickedItems,
+} from "./utils";
 
 const Rating = ({ initialRating }: RatingProps) => {
   const [hover, setHover] = useState(false);
@@ -9,60 +15,47 @@ const Rating = ({ initialRating }: RatingProps) => {
   const [hoverRating, setHoverRating] = useState(initialRating);
 
   const onClick = (id: number) => {
-    const itemIndex = rating.findIndex((itemArg) => itemArg.id === id);
+    const itemIndex = rating.findIndex((item) => findItemIndex(item, id));
     const isActive = rating[itemIndex]["active"];
     const isClicked = rating[itemIndex]["clicked"];
 
     if (isActive && id === 5) {
-      return setRating((prev) =>
-        prev.map((ratingItem) => ({ ...ratingItem, active: false }))
-      );
+      return setRating((prev) => prev.map(resetActive));
     }
 
     if (isActive && id !== 1) {
       if (isClicked) {
-        return setRating((prev) =>
-          prev.map((ratingItem) => ({
-            ...ratingItem,
-            active: false,
-            clicked: false,
-          }))
-        );
+        return setRating((prev) => prev.map(resetClickedAndActive));
       }
 
       return setRating((prev) => [
-        ...prev.slice(0, itemIndex + 1).map((ratingItem) => ({
-          ...ratingItem,
-          active: true,
-          clicked: id === ratingItem.id,
-        })),
-        ...prev.slice(itemIndex + 1).map((ratingItem) => ({
-          ...ratingItem,
-          active: false,
-          clicked: id === ratingItem.id,
-        })),
+        ...prev
+          .slice(0, itemIndex + 1)
+          .map((item) => setClickedItems(item, id, true)),
+        ...prev
+          .slice(itemIndex + 1)
+          .map((item) => setClickedItems(item, id, false)),
       ]);
     }
 
     setRating((prev) => [
-      ...prev.slice(0, itemIndex + 1).map((ratingItem) => ({
-        ...ratingItem,
-        active: true,
-        clicked: id === ratingItem.id,
-      })),
-      ...prev.slice(itemIndex + 1),
+      ...prev
+        .slice(0, itemIndex + 1)
+        .map((item) => setClickedItems(item, id, true)),
+      ...prev
+        .slice(itemIndex + 1)
+        .map((item) => setClickedItems(item, id, false)),
     ]);
   };
 
   const onMouseEnter = (id: number) => {
     setHover(true);
 
-    const hoveredItemIndex = rating.findIndex((itemArg) => itemArg.id === id);
-    const isActive = rating[hoveredItemIndex]["active"];
-    const isClicked = rating[hoveredItemIndex]["clicked"];
+    const hoveredItemIndex = rating.findIndex((item) =>
+      findItemIndex(item, id)
+    );
     const lastActiveItemIndex =
       rating.filter((itemArg) => itemArg.active).length - 1;
-    console.log({ lastActiveItemIndex, hoveredItemIndex });
 
     if (lastActiveItemIndex === hoveredItemIndex) {
       return setHoverRating(rating);
@@ -70,27 +63,21 @@ const Rating = ({ initialRating }: RatingProps) => {
 
     if (hoveredItemIndex > lastActiveItemIndex) {
       return setHoverRating(() => [
-        ...rating.slice(0, hoveredItemIndex + 1).map((ratingItem) => ({
-          ...ratingItem,
-          active: true,
-          clicked: id === ratingItem.id,
-        })),
+        ...rating
+          .slice(0, hoveredItemIndex + 1)
+          .map((item) => setClickedItems(item, id, true)),
         ...rating.slice(hoveredItemIndex + 1),
       ]);
     }
 
     if (hoveredItemIndex < lastActiveItemIndex) {
       return setHoverRating(() => [
-        ...rating.slice(0, hoveredItemIndex + 1).map((ratingItem) => ({
-          ...ratingItem,
-          active: true,
-          clicked: id === ratingItem.id,
-        })),
-        ...rating.slice(hoveredItemIndex + 1).map((ratingItem) => ({
-          ...ratingItem,
-          active: false,
-          clicked: id === ratingItem.id,
-        })),
+        ...rating
+          .slice(0, hoveredItemIndex + 1)
+          .map((item) => setClickedItems(item, id, true)),
+        ...rating
+          .slice(hoveredItemIndex + 1)
+          .map((item) => setClickedItems(item, id, false)),
       ]);
     }
   };

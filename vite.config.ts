6b56d3140eac9b0916b6c81937db6a2c80199f93
j0sephh123 <iossif123gg@gmail.com>
2022-04-26 08:22:23 +0300

    fix build for docusaurus

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        docs: resolve(__dirname, "docs/index.html"),
+        docs: resolve(__dirname, "docs/build/index.html"),
       },
     },
   },
